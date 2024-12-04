6eb716ea6c614072ee91bbfb207a142268758ccd
bharatkumar231131 <bharatkumarwd@gmail.com>
2024-12-04 16:09:38 +0530

    update new build updload

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+
+class AdminController extends Controller
+{
+    public function login(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+
+            $rules = [
+                'email'    => 'required|email|max:255',
+                'password' => 'required',
+            ];
+
+            $customMessages = [
+                'email.required'    => 'Email Address is required!',
+                'email.email'       => 'Valid Email Address is required',
+                'password.required' => 'Password is required!',
+            ];
+
+            $request->validate($rules, $customMessages);
+
+            if (Auth::attempt(['email' => $data['email'], 'password' => $data['password']])) {
+                $user = Auth::user();
+
+                if ($user->type == 'admin' && $user->status == '0') {
+                    Auth::logout();
+                    return redirect()->back()->with('error_message', 'Your admin account is not active');
+                } elseif ($user->type == 'superadmin' || $user->type == 'admin') {
+                    return redirect('/admin/dashboard');
+                } else {
+                    Auth::logout();
+                    return redirect()->back()->with('error_message', 'Unauthorized access');
+                }
+            } else {
+                return redirect()->back()->with('error_message', 'Invalid Email or Password');
+            }
+        }
+
+        if (Auth::check()) {
+            return redirect('/admin/dashboard');
+        }
+
+        return view('admin.login');
+    }
+
+    public function dashboard()
+    {
+        return view('admin.index');
+    }
+}
