1b4a923b9e6feb3fcfda5aa7c423dda505338797
harsh-vardhan-pareek <pareekh611@gmail.com>
2024-12-09 15:59:25 +0530

    update

--- app/Http/Controllers/Front/InquiryController.php
+++ app/Http/Controllers/Front/InquiryController.php
@@ -19,6 +19,34 @@ class InquiryController extends Controller
     public function saveInquiry(Request $request)
     {
         if ($request->isMethod('post')) {
+
+            $data = $request->all();
+
+
+            $rules = [
+                'name' => 'required',
+                'name' => 'email',
+                'name' => 'j',
+                'name' => 'required',
+                'name' => 'required',
+
+            ];
+
+            $customMessages = [
+                'name.required' => 'Package Name is required.',
+                'number_of_section.integer' => 'Number of sections must be an integer.',
+                'number_of_category.integer' => 'Number of categories must be an integer.',
+                'number_of_product.integer' => 'Number of products must be an integer.',
+                'price.numeric' => 'Price must be a valid number.',
+                'days.numeric' => 'Price must be a valid number.',
+            ];
+
+            $validator = Validator::make($data, $rules, $customMessages);
+
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+
             Inquiry::create([]);
         }
     }
