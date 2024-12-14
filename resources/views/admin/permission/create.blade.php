56161709b502e98a80cdbde31bfe20ae16bd4b7f
harsh-vardhan-pareek <pareekh611@gmail.com>
2024-12-14 10:28:25 +0530

    update

--- resources/views/admin/permission/create.blade.php
+++ resources/views/admin/permission/create.blade.php
@@ -35,63 +35,62 @@
 
 @extends('components.admin.layouts')
 @section('content')
-    <div class="container">
-        <div class="py-5">
-            <div class="row g-4 align-items-center">
-                <div class="col">
-                    <nav class="mb-2" aria-label="breadcrumb">
-                        <ol class="breadcrumb breadcrumb-sa-simple">
-                            <li class="breadcrumb-item"><a href="{{ url('admin/dashboard') }}">Dashboard</a></li>
-                            <span>&nbsp;/&nbsp;</span>
-                            <li class="breadcrumb-item"><a href="{{ route('permissions.index') }}">Permissions</a></li>
-                            <span>&nbsp;/&nbsp;</span>
-                            <li class="breadcrumb-item active">Create Permission</li>
-                        </ol>
-                    </nav>
-                    <h1 class="h3 m-0">Create Permission</h1>
-                </div>
+<div class="container">
+    <div class="py-5">
+        <div class="row g-4 align-items-center">
+            <div class="col">
+                <nav class="mb-2" aria-label="breadcrumb">
+                    <ol class="breadcrumb breadcrumb-sa-simple">
+                        <li class="breadcrumb-item"><a href="{{ url('admin/dashboard') }}">Dashboard</a></li>
+                        <span>&nbsp;/&nbsp;</span>
+                        <li class="breadcrumb-item"><a href="{{ route('permissions.index') }}">Permissions</a></li>
+                        <span>&nbsp;/&nbsp;</span>
+                        <li class="breadcrumb-item active">Create Permission</li>
+                    </ol>
+                </nav>
+                <h1 class="h3 m-0">Create Permission</h1>
             </div>
         </div>
+    </div>
 
-        <div class="mb-5">
-            <div class="row">
-                <div class="col-12 col-lg-6">
-                    <div class="card">
-                        <div class="card-body">
-                            @if (Session::has('error_message'))
-                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                    <strong>Error:</strong> {{ Session::get('error_message') }}
-                                    <button type="button" class="sa-close" data-bs-dismiss="alert" aria-label="Close">
-                                    </button>
-                                </div>
-                            @endif
-
-                            @if (Session::has('success_message'))
-                                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                    <strong>Success:</strong> {{ Session::get('success_message') }}
-                                    <button type="button" class="sa-close" data-bs-dismiss="alert" aria-label="Close">
-                                    </button>
-                                </div>
-                            @endif
-
-                            <form action="{{ route('permissions.store') }}" method="post">
-                                @csrf
-                                <div class="mb-4">
-                                    <label class="form-label" for="name">Permission Name</label>
-                                    <input type="text" class="form-control" id="name"
-                                        placeholder="Enter Permission Name" name="name" value="{{ old('name') }}"
-                                        required>
-                                    @error('name')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
-                                </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                                <a href="{{ route('permissions.index') }}" class="btn btn-secondary">Cancel</a>
-                            </form>
+    <div class="mb-5">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <div class="card">
+                    <div class="card-body">
+                        @if (Session::has('error_message'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong>Error:</strong> {{ Session::get('error_message') }}
+                            <button type="button" class="sa-close" data-bs-dismiss="alert" aria-label="Close">
+                            </button>
                         </div>
+                        @endif
+
+                        @if (Session::has('success_message'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>Success:</strong> {{ Session::get('success_message') }}
+                            <button type="button" class="sa-close" data-bs-dismiss="alert" aria-label="Close">
+                            </button>
+                        </div>
+                        @endif
+
+                        <form action="{{ route('permissions.store') }}" method="post" data-parsley-validate>
+                            @csrf
+                            <div class="mb-4">
+                                <label class="form-label" for="name">Permission Name</label>
+                                <input type="text" class="form-control" id="name" placeholder="Enter Permission Name" name="name"
+                                    value="{{ old('name') }}" required>
+                                @error('name')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <a href="{{ route('permissions.index') }}" class="btn btn-secondary">Cancel</a>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
